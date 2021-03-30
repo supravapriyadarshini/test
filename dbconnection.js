e6e1b45e7cdd2c62611d7e0a2b203d17bdf8755c
supravapriyadarshini <54208864+supravapriyadarshini@users.noreply.github.com>
2021-03-30 22:39:36 +0530

    task2

---
+++ dbconnection.js
@@ -0,0 +1,10 @@
+[code language = "javascript"]  
+var mysql = require('mysql');  
+var connection = mysql.createPool({  
+    host: 'localhost',  
+    user: 'root',  
+    password: '',  
+    database: 'demo'  
+});  
+module.exports = connection;  
+[/code]  
